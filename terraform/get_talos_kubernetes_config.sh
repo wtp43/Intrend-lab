c0dcf8849009b09ac6984a3cfb88fcd451983b85
William Tang <wjt40398@gmail.com>
2024-10-19 14:21:36 -0400

    feat: infra WIP

--- terraform/get_talos_kubernetes_config.sh
+++ terraform/get_talos_kubernetes_config.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
-mkdir ~/.kube/
+mkdir -p ~/.kube/
+mkdir -p ~/.talos/
 terraform output -raw kube_config >~/.kube/config
-
-mkdir ~/.talos/
 terraform output -raw talos_config >~/.talos/config
+chmod 600 ~/.kube/config
+chmod 600 ~/.talos/config
